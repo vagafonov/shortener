cc36169fce23c4c55fd2957b60c0dab779a18443
agafonov <v.agafonov@webinar.ru>
2024-03-09 21:05:35 +0300

    Increment 5

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/caarlos0/env/v6"
 	"github.com/vagafonov/shrinkr/config"
 	"github.com/vagafonov/shrinkr/internal/application"
 	"github.com/vagafonov/shrinkr/pkg/storage"
+	"log"
 )
+
+var options struct {
+	ServerURL string `env:"SERVER_ADDRESS"`
+	ResultURL string `env:"BASE_URL"`
+}
 
 func main() {
 	parseFlags()
+	parseEnv()
 	cnt := application.NewContainer(
-		config.NewConfig(flags.serverURL, flags.resultURL),
+		config.NewConfig(options.ServerURL, options.ResultURL),
 		storage.NewMemoryStorage(),
 	)
 	app := application.NewApplication(cnt)
 	app.Serve()
+}
+
+func parseFlags() {
+	flag.StringVar(&options.ServerURL, "a", "localhost:8080", "address and port to run server")
+	flag.StringVar(&options.ResultURL, "b", "http://localhost:8080", "address and port for result short url")
+	flag.Parse()
+}
+
+func parseEnv() {
+	err := env.Parse(&options)
+	fmt.Println(options)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/vagafonov/shrinkr
 
 go 1.21.5
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/stretchr/testify v1.9.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-chi/chi/v5 v5.0.12 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
